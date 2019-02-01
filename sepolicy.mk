bb4dbb20a63d08f97be3a822717690bc36edc19b
Tri Vo <trong@google.com>
2019-02-01 11:27:25 -0800

    common: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,7 +1,7 @@
 # Common amlogic policy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/amlogic/sepolicy/common/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/amlogic/sepolicy/common/public
 BOARD_VENDOR_SEPOLICY_DIRS += device/amlogic/sepolicy/common/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/amlogic/sepolicy/common/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/amlogic/sepolicy/common/public
 
 ifneq ($(filter g12a g12b sm1,$(TARGET_AMLOGIC_SOC)),)
 BOARD_VENDOR_SEPOLICY_DIRS += device/amlogic/sepolicy/g12/vendor
