47f0c355b6ae48788b8c72af43f9b1723b35e051
Christian Hoffmann <chrmhoffmann@gmail.com>
2022-06-12 12:39:38 +0200

    amlogic/sepolicy: Fix blur_sysprop_notifier policies
    Change-Id: I3f7a33613ca2c55176c730a93adac7c1b9c091cf

--- sepolicy.mk
+++ sepolicy.mk
@@ -2,6 +2,7 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/amlogic/sepolicy/common/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/amlogic/sepolicy/common/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += device/amlogic/sepolicy/common/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/amlogic/sepolicy/common/system_ext/private
 
 ifneq ($(filter g12a g12b sm1,$(TARGET_AMLOGIC_SOC)),)
 BOARD_VENDOR_SEPOLICY_DIRS += device/amlogic/sepolicy/g12/vendor
